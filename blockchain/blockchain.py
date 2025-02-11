ed0799945b7c2370ea2542bfeeba4a103e566cf6
BryanHuaytan <huaytanbryan@gmail.com>
2025-02-10 21:22:41 -0500

    nuevos cambiosss

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,27 +1,46 @@
 import datetime
 import hashlib
 import json
+import os
+import subprocess
+import threading
 import requests
 from flask import Flask, jsonify, request
 
 # Configuración de Flask
 app = Flask(__name__)
 
-# API de IPFS (debe estar corriendo el daemon)
-IPFS_API = "http://127.0.0.1:5001/api/v0"
+# API base de IPFS
+IPFS_API_BASE = "http://127.0.0.1:{}/api/v0"
+NODO_PUERTOS = [5001, 5002, 5003, 5004, 5005]
 
 class IPFSManager:
-    "Clase para manejar la conexión con IPFS"
-    @staticmethod
-    def subir_archivo(file):
+    """Clase para manejar la conexión con IPFS y la gestión de nodos."""
+    def __init__(self, puerto):
+        self.ipfs_api = IPFS_API_BASE.format(puerto)
+        self.puerto = puerto
+        self.iniciar_ipfs()
+
+    def iniciar_ipfs(self):
+        """Verifica e inicia IPFS automáticamente."""
+        if not os.path.exists(os.path.expanduser("~/.ipfs")):
+            subprocess.run(["ipfs", "init"], check=True)
+        subprocess.Popen(["ipfs", "daemon", "--api-port", str(self.puerto)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    def subir_archivo(self, file):
+        """Sube un archivo a IPFS."""
         files = {'file': (file.filename, file.stream, file.content_type)}
-        response = requests.post(IPFS_API + "/add", files=files)
+        response = requests.post(self.ipfs_api + "/add", files=files)
         if response.status_code == 200:
             return response.json()["Hash"]
         return None
 
-class BlockchainManager:
-    "Clase para manejar la cadena de bloques"
+    def conectar_nodo(self, direccion_peer):
+        """Conecta el nodo actual a otro nodo IPFS."""
+        requests.post(self.ipfs_api + f"/swarm/connect?arg=/ip4/127.0.0.1/tcp/{direccion_peer}")
+
+class Blockchain:
+    """Clase que maneja la blockchain."""
     def __init__(self):
         self.chain = []
         self.crear_bloque(prueba=1, hash_anterior='0', documento=None)
@@ -55,25 +74,18 @@ class BlockchainManager:
         bloque_codificado = json.dumps(bloque, sort_keys=True).encode()
         return hashlib.sha256(bloque_codificado).hexdigest()
 
-    def es_cadena_valida(self, cadena):
-        bloque_anterior = cadena[0]
-        indice_bloque = 1
-        while indice_bloque < len(cadena):
-            bloque = cadena[indice_bloque]
-            if bloque['hash_anterior'] != self.calcular_hash(bloque_anterior):
-                return False
-            prueba_anterior = bloque_anterior['prueba']
-            prueba = bloque['prueba']
-            operacion_hash = hashlib.sha256(str(prueba**2 - prueba_anterior**2).encode()).hexdigest()
-            if operacion_hash[:4] != '0000':
-                return False
-            bloque_anterior = bloque
-            indice_bloque += 1
-        return True
+# Inicializar Blockchain y Nodos IPFS
+blockchain = Blockchain()
+nodos_ipfs = [IPFSManager(puerto) for puerto in NODO_PUERTOS]
 
-# Inicializar blockchain y gestor de IPFS
-blockchain = BlockchainManager()
-ipfs_manager = IPFSManager()
+# Conectar nodos entre sí
+def conectar_nodos():
+    for i in range(len(nodos_ipfs)):
+        for j in range(len(nodos_ipfs)):
+            if i != j:
+                nodos_ipfs[i].conectar_nodo(NODO_PUERTOS[j])
+
+threading.Thread(target=conectar_nodos).start()
 
 @app.route("/subir_archivo", methods=['POST'])
 def subir_archivo():
@@ -81,7 +93,7 @@ def subir_archivo():
         return jsonify({'error': 'No se encontró ningún archivo'}), 400
 
     file = request.files['file']
-    ipfs_hash = ipfs_manager.subir_archivo(file)
+    ipfs_hash = nodos_ipfs[0].subir_archivo(file)  # Usa el primer nodo para subir el archivo
     
     if ipfs_hash:
         documento = {'contenido': ipfs_hash, 'tipo': file.content_type, 'fecha': str(datetime.datetime.now())}
